b47f0c805bc5ed774213eb326ce6b97a89d4631e
Sujit Thonse <thonse.sujit@gmail.com>
2020-04-01 11:58:25 +0200

    Add the Web API Project
    Adds a blank WebAPI project to TimCo

---
+++ TRMDataManager/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            //wraped in ok . now we have additional option what to send back. but include this in documation. by using IHttpActionResult. but for this project its kept empty.
+            //return Ok(new string[] { "value1", "value2" , userId });
+            return new string[] { "value1", "value2", userId };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/52
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
