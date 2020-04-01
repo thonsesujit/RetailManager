1a41998ef2b72af8b3ffb8ed6c710f74b8c5f55c
Sujit Thonse <thonse.sujit@gmail.com>
2020-04-01 17:58:14 +0200

    Add SwaggerDcutaaion to API
    Adds Swashbuckle / swagger to the api for documentation and testing

---
+++ TRMDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,57 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            // adding toke rout
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    //auth catogary. and post command . type of  data should come from "application/x-www-form-urlencoded"
+
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                            
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = true,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = true,
+                            @in = "formData"
+                        }
+                    }
+                }
+
+            });
+        }
+    }
+}
