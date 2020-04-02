5d47df70af7641f4d3de27e6292f27875ef5a3d2
Sujit Thonse <thonse.sujit@gmail.com>
2020-04-02 09:21:54 +0200

    Added WPF / Calibre Micro
    Added Calibre micro and made folders for MVVM

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //on startup we gonna start ViewModel. 
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
