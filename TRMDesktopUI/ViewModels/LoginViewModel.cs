3c914905519496e277b4bd6d42351e6c3230dbd4
Sujit Thonse <thonse.sujit@gmail.com>
2020-04-30 10:56:30 +0200

    12Loginform error handling
    Throws out error in the login screen is the username and password does not match accoording to the api call.

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -43,6 +43,34 @@ namespace TRMDesktopUI.ViewModels
         }
 
 
+        public bool IsErrorVisible
+        {
+            get {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+            return output;
+            }
+  
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+
+            }
+        }
+
 
         public bool CanLogIn
         {
@@ -58,17 +86,18 @@ namespace TRMDesktopUI.ViewModels
         }
 
 
-
+        // calling and api calls are asynchronous
         public async Task LogIn()
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
 
