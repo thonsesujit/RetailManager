f451aedfcc065f850f8f278bbf12734a43a23bb4
Sujit Thonse <thonse.sujit@gmail.com>
2020-04-30 14:35:02 +0200

    13User data access to SQL database

---
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library.Internal.DataAccess
+{
+    //anything outside library should not have accesst to this methods. they need to access through UserData
+    internal class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString; // passing name of our connection string and returns connection string.  its gonna get he webconfig from TRMDdataManager
+        }
+        //Dapper is a microORM. object relational mapper. it allows us to talk to database. get information back and maps information to object. it very fast. U is generic. i can call it any letter.
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using(IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName) //in generic common type is T , then we use U V W ...
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(storedProcedure, parameters, commandType: CommandType.StoredProcedure);   // infuture we can make this execute into async
+
+            }
+        }
+    }
+
+}
---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+
+        // GET: User/GetById
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId(); // getting user Id from user who is logged in. 
+            UserData data = new UserData();  // api model is display models and library is data access model. you might add some attribute to display model. we use automapper to add true speration.
+            return data.GetUserById(userId);
+             
+        }
+
+        
+    }
+}
