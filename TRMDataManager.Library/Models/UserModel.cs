57e8cb85e05fb589545429299f92307d53e685de
Sujit Thonse <thonse.sujit@gmail.com>
2020-05-01 11:14:15 +0200

    Solved Bugs in user model

--- TRMDataManager.Library/Models/UserModel.cs
+++ TRMDataManager.Library/Models/UserModel.cs
@@ -9,7 +9,7 @@ namespace TRMDataManager.Library.Models
     public class UserModel
     {
         public string Id { get; set; }
-        public string FistName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public DateTime CreatedDate { get; set; }
